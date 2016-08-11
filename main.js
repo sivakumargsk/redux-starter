f223f0837578c9c2a81d0e0092c377ca34a776d6
sivakumargsk <geddasiva777@gmail.com>
2016-08-12 00:46:15 +0530

    simple UI is designed

---
+++ main.js
@@ -0,0 +1,7 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+import TodoManager from './components/todoManager.js';
+
+ReactDOM.render(<TodoManager />,
+ document.getElementById('content'));
